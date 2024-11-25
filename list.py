e993adbc2f52805cd21c76191c3ded272fadb127
Tanujairam <88611197+TanujairamV@users.noreply.github.com>
2024-11-25 21:46:48 +0530

    Update list.py

--- list.py
+++ list.py
@@ -11,7 +11,7 @@ while True:
         item = int(input("Enter item: "))
         position = int(input("In which position: "))
         index = position - 1
-        l.insert(index, item)
+        list.insert(index, item)
         print("Your new list is", list)
     elif choice == 2:
         print("Deletion menu")
@@ -22,16 +22,16 @@ while True:
         dchoice = int(input("Enter a choice: "))
         if dchoice == 1:
             item = int(input("Item to be deleted: "))
-            l.remove(item)
+            list.remove(item)
             print("List:", list)
         elif dchoice == 2:
             index = int(input("Index to be removed: "))
-            l.pop(index)
+            list.pop(index)
             print("List:", list)
         elif dchoice == 3:
             low = int(input("Lower limit to be removed: "))
             high = int(input("Higher limit to be removed: "))
-            del l[low:high]
+            del list[low:high]
             print("List:", list)
         else:
             print("Enter valid numbers")
